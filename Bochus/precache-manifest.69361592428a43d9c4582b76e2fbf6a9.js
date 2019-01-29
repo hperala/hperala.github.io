b5ec4ea57f5cbf4a212b9f39903c2c95bb9a122a
Harri Perälä <harri.perala@iki.fi>
2019-01-29 15:12:38 +0200

    Update Bochus client

---
+++ Bochus/precache-manifest.69361592428a43d9c4582b76e2fbf6a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f3f66f6d8588d5e57296",
+    "url": "./static/js/main.f3f66f6d.chunk.js"
+  },
+  {
+    "revision": "5e08eed5abc2710c6e49",
+    "url": "./static/js/1.5e08eed5.chunk.js"
+  },
+  {
+    "revision": "f3f66f6d8588d5e57296",
+    "url": "./static/css/main.9a45b6f1.chunk.css"
+  },
+  {
+    "revision": "5e08eed5abc2710c6e49",
+    "url": "./static/css/1.cae4d7d1.chunk.css"
+  },
+  {
+    "revision": "626d112004b72df40d7fc0f2efc8b1fc",
+    "url": "./index.html"
+  }
+];
